88da47eb2cb21e25644ff57d2eb6e6842f930ab6
SpectCOW <126259962+SpcFORK@users.noreply.github.com>
2024-05-10 03:42:59 +0000

    Very neat, very neato  too

--- src/deCompress.js
+++ src/deCompress.js
@@ -1,19 +1,51 @@
 import fs, { fdatasync } from "node:fs";
 import brotli from "brotli";
 import { decrypt } from "node-encryption";
+
+import { muint8 } from "gomooe";
+const { UInt8E } = muint8;
+
+const removeLastExt = (f = "") => {
+  const lastIndex = f.lastIndexOf(".");
+  return {
+    filename: f.slice(0, lastIndex),
+    ext: f.slice(lastIndex + 1),
+  };
+};
+
+/**
+ * @param {string} file
+ * @param {muint8.MUint8} encoder
+ * @param {*} fileData
+ * @param {string} password
+ */
 export default function deCompress(file, encoder, fileData, password) {
-  let finalFile = file.slice(0, -3);
+  let finalFile = removeLastExt(file).filename;
+
   console.log(`${file} -> ${finalFile}`);
+
   let data = fileData;
-  if (password != undefined) {
+  if (password !== undefined)
     data = Buffer.from(decrypt(data.toString(), password));
-  }
+
   console.log(data.toString());
   console.log(typeof data);
-  let brDecode = brotli.decompress(fileData);
-  let unEncodedFile = encoder.decode(brDecode);
-  fs.writeFile(finalFile, unEncodedFile, (err) => {
+
+  let brDecode = brotli.decompress(data);
+  let unEncodedStr = UInt8E.encodeUint8(brDecode);
+  let unEncodedFile = encoder.decode(unEncodedStr);
+
+  let lastName = finalFile;
+  // let dupeId = 1;
+  // let { filename, ext } = removeLastExt(finalFile);
+  // while (fs.existsSync(lastName)) {
+  //   lastName = `${filename} (${dupeId}).${ext}`;
+  //   dupeId++;
+  // }
+
+  fs.writeFile(lastName, unEncodedFile, (err) => {
     if (err) throw err;
   });
+
   console.log("Done!");
 }
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import eobj from "gomooe/muint8/muint8.js";
+import { muint8 } from "gomooe";
 
 import fs from "node:fs";
 
@@ -6,24 +6,21 @@ import compress from "./compress.js";
 import deCompress from "./deCompress.js";
 
 export default async function main() {
-  let encoder = new eobj.MUint8Encoder();
+  const encoder = new muint8.MUint8Encoder();
+  
   const file = process.argv[2];
   const password = process.argv[3];
+
   if (file == undefined) {
     console.log("Usage: forc <file> [password]");
     process.exit(1);
   }
-  let fileData;
+
   fs.readFile(process.cwd() + "/" + file, (err, data) => {
     if (err) throw err;
-    if (data) {
-      fileData = data;
-      if (file.endsWith(".fc")) {
-        deCompress(file, encoder, fileData, password);
-      }
-      if (!file.endsWith(".fc")) {
-        compress(file, encoder, fileData, password);
-      }
-    }
+    
+    if (data)
+      if (file.endsWith(".fc")) deCompress(file, encoder, data, password);
+      else compress(file, encoder, data, password);
   });
 }
